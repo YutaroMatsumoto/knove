62102f0006f7392c74855ab04f5c994b7aaa6a7b
YutaroMatsumoto <spurs03170629@gmail.com>
2022-07-29 19:44:01 +0900

    update: テーブル名を複数形にする実装をコメントアウト

--- be/src/config/db/customNamingStrategy.ts
+++ be/src/config/db/customNamingStrategy.ts
@@ -1,47 +1,48 @@
-import * as pluralize from 'pluralize';
-import { DefaultNamingStrategy, NamingStrategyInterface } from 'typeorm';
-import { snakeCase } from 'typeorm/util/StringUtils';
+import * as pluralize from 'pluralize'
+import { DefaultNamingStrategy, NamingStrategyInterface } from 'typeorm'
+import { snakeCase } from 'typeorm/util/StringUtils'
 
 export class CustomNamingStrategy
   extends DefaultNamingStrategy
   implements NamingStrategyInterface
 {
-  tableName(targetName: string, userSpecifiedName: string): string {
-    return userSpecifiedName
-      ? userSpecifiedName
-      : pluralize.plural(snakeCase(targetName));
-  }
+  // NOTE: 以下のコメントアウトを外すと、複数形のテーブル名になる
+  // tableName(targetName: string, userSpecifiedName: string): string {
+  //   return userSpecifiedName
+  //     ? userSpecifiedName
+  //     : pluralize.plural(snakeCase(targetName));
+  // }
 
   columnName(
     propertyName: string,
     customName: string,
-    embeddedPrefixes: string[],
+    embeddedPrefixes: string[]
   ): string {
-    return customName ? customName : snakeCase(propertyName);
+    return customName ? customName : snakeCase(propertyName)
   }
 
   joinColumnName(relationName: string, referencedColumnName: string) {
     return snakeCase(
-      pluralize.singular(relationName) + '_' + referencedColumnName,
-    );
+      pluralize.singular(relationName) + '_' + referencedColumnName
+    )
   }
 
   joinTableName(
     firstTableName: string,
     secondTableName: string,
     firstPropertyName: string,
-    secondPropertyName: string,
+    secondPropertyName: string
   ) {
-    return snakeCase(firstTableName + '_' + secondTableName);
+    return snakeCase(firstTableName + '_' + secondTableName)
   }
 
   joinTableColumnName(
     tableName: string,
     propertyName: string,
-    columnName: string,
+    columnName: string
   ) {
     return snakeCase(
-      pluralize.singular(tableName) + '_' + (columnName || propertyName),
-    );
+      pluralize.singular(tableName) + '_' + (columnName || propertyName)
+    )
   }
 }
